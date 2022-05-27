465516698bf55d2562fd0afaa039daa2e9ec4a0a
21007383 <21007383@172.29.85.189>
2022-05-27 14:33:50 +0800

    First Push to P00 repository

---
+++ c206_P00/src/Calculator.java
@@ -0,0 +1,31 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21007383, 27 May 2022 11:30:50 am
+ */
+
+/**
+ * @author 21007383
+ *
+ */
+public class Calculator {
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiple(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a / b;
+	}
+
+}
